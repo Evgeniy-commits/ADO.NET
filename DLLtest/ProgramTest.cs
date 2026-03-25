aca730db4fb8be88f52cc8614b4301ce312148cb
Admin <Admin@DESKTOP-8URQBFL>
2026-03-25 23:02:27 +0500

    ConfigurationManager DONE

--- DLLtest/ProgramTest.cs
+++ DLLtest/ProgramTest.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DBtools;
+//using DBtools;
+using System.Configuration;
 
 namespace DLLtest
 {
@@ -11,47 +12,17 @@ namespace DLLtest
 	{
 		static void Main(string[] args)
 		{
-			//string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;" +
-			//						   "Initial Catalog=Movies_PV_521;" +
-			//						   "Integrated Security=True;" +
-			//						   "Connect Timeout=30;Encrypt=False;" +
-			//						   "TrustServerCertificate=False;" +
-			//						   "ApplicationIntent=ReadWrite;" +
-			//						   "MultiSubnetFailover=False";
-
-			//Connector connector = new Connector(connection_string);
-
-			//Console.WriteLine(connector.GetPrimaryKeyColumnName("Directors"));
-			//Console.WriteLine(connector.GetPrimaryKeyColumnName("Movies"));
-
-			////connector.Insert($@"INSERT Directors (director_id, first_name, last_name)
-			////		VALUES ({connector.GetNextPrimaryKey("Directors")}, N'Guy', N'Richie');");
-			//connector.Insert
-			//	(
-			//	"Directors",
-			//	"director_id,first_name,last_name",
-			//	$"{connector.GetNextPrimaryKey("Directors")},John,Singleton"
-			//	);
-
-			//Console.WriteLine($"PK Max:\t{connector.GetMaxPrimaryKey("Directors")}");
-
-			//string cmd =
-			//@"SELECT movie_id, title, release_date, first_name, last_name 
-			//FROM Movies, Directors WHERE director = director_id";
-			////connector.Select(cmd);
-			//connector.Select("*", "Directors");
-			//Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
-			////connector.Select("SELECT * FROM Directors");
-			//connector.Select
-			//	(
-			//		"title, release_date, first_name, last_name",
-			//		"Movies, Directors",
-			//		"director = director_id"
-			//	);
-			//Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Movies")}");
-
-			Connector connector = new Connector("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+			DBtools.Connector connector = new DBtools.Connector
+				(
+				ConfigurationManager.ConnectionStrings["Movies_PV_521"].ConnectionString
+				);
 			connector.Select("SELECT * FROM Directors");
+
+			DBtools.Connector academy_connector = new DBtools.Connector
+				(
+				ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString
+				);
+			academy_connector.Select("SELECT * FROM Disciplines");
 		}
 	}
 }
