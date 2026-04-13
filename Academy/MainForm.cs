9a0ff534dd65979226424aad7b6ede9df8df2456
Admin <Admin@DESKTOP-8URQBFL>
2026-04-13 20:18:53 +0500

    StudentForm in progress

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -45,6 +45,7 @@ namespace Academy
 		Dictionary<string, int> d_directions;
 		Dictionary<string, int> d_groups;
 
+		StudentForm studentForm;
 
 		public MainForm()
 		{
@@ -99,6 +100,12 @@ namespace Academy
 					$" AND direction = {d_directions[cbStudentsDirection.SelectedItem.ToString()]}"
 				);
 			toolStripStatusLabel.Text = $"{status_messages[0]}: {dgvStudents.RowCount - 1}";
+		}
+
+		private void buttonAddStudent_Click(object sender, EventArgs e)
+		{
+			studentForm = new StudentForm();
+			studentForm.ShowDialog();
 		}
 	}
 }
