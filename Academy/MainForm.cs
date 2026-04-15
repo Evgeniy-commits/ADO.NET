61391419f423fb07fc4d68f69f4422c70dbe94da
Admin <Admin@DESKTOP-8URQBFL>
2026-04-15 17:11:30 +0500

    Student Double Click Done

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -17,8 +17,8 @@ namespace Academy
 		{
 			new Query
 				(
-					"last_name, first_name, middle_name, group_name, direction_name",
-					"Students, Groups, Directions",
+					"stud_id,last_name,first_name,middle_name,group_name,direction_name",
+					"Students,Groups,Directions",
 					"[group] = group_id AND direction = direction_id"
 				),
 			new Query
@@ -107,6 +107,12 @@ namespace Academy
 			studentForm = new StudentForm();
 			if (studentForm.ShowDialog() == DialogResult.OK)
 				tabControl_SelectedIndexChanged(tabControl, null);
+		}
+
+		private void dgvStudents_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+		{
+			StudentForm form = new StudentForm(Convert.ToInt32(dgvStudents.Rows[e.RowIndex].Cells["stud_id"].Value));
+			form.ShowDialog();
 		}
 	}
 }
